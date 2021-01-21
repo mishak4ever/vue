b1e4fd85040449e45fb55984f2a14b4040416fed
Mikhail Kolyasnikov <mishak4ever@gmail.com>
2021-01-21 22:49:08 +0500

    компонент скилов

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,8 @@
-console.log('this is the skills module');
+console.log("this is the skills module");
+
+import Vue from "vue";
+
+new Vue({
+  el: "#skills_component",
+  template: "#skills_list",
+});
