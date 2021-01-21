f0d5e8ab20b44b2165be86813ef1df64c6dfbeb4
Mikhail Kolyasnikov <mishak4ever@gmail.com>
2021-01-22 00:10:07 +0500

    значение скилов с JSON

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,8 +1,41 @@
-console.log("this is the skills module");
+// console.log("this is the skills module");
 
 import Vue from "vue";
+
+const skillItem = {
+  props: ["skill"],
+  template: "#skills_item",
+  mounted() {
+    const circle = this.$refs["computed_circle"];
+    const dashArray = parseInt(
+      getComputedStyle(circle).getPropertyValue("stroke-dasharray")
+    );
+    const percent = (dashArray / 100) * (100 - this.skill.percent);
+    circle.style.setProperty ('stroke-dashoffset',percent);
+    console.log(percent);
+  },
+};
+
+const skillRow = {
+  props: ["category"],
+  template: "#skills_row",
+  components: {
+    skillItem,
+  },
+};
 
 new Vue({
   el: "#skills_component",
   template: "#skills_list",
+  components: {
+    skillRow,
+  },
+  data() {
+    return {
+      skills: [],
+    };
+  },
+  created() {
+    this.skills = require("../json/skills.json");
+  },
 });
