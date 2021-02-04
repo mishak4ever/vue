bd5c9733b4659167ff72c35019d5d133d293e90d
Mikhail Kolyasnikov <mishak4ever@gmail.com>
2021-02-05 00:10:31 +0500

    fix ошибки в модуле axios

--- src/admin/axios.js
+++ src/admin/axios.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://webdev-api.loftschool.com"
 
-const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjQzMCwiaXNzIjoiaHR0cDovL3dlYmRldi1hcGkubG9mdHNjaG9vbC5jb20vbG9naW4iLCJpYXQiOjE2MTI0NjIwMjcsImV4cCI6MTYxMjQ2MjA4NywibmJmIjoxNjEyNDYyMDI3LCJqdGkiOiJKajZxTlZlZmpyN2FMSVdnIn0.QdFwGGnsRNgimqCo73lpipf7UzwDtGMHi3kEnw8u_DE";
-// const token = localStorage.getItem("token");
+const token = localStorage.getItem("token");
 
 if (token) {
   axios.defaults.headers["Authorization"] = `Bearer ${token}`;
